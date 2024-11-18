d458295df895fa2af7ac5156a8f6ed7db9adf804
md shahid iqbal <111365870+iqbalshaid@users.noreply.github.com>
2024-11-18 13:51:02 +0530

    Update Incubation.js

--- src/components/Mission/Incubation.js
+++ src/components/Mission/Incubation.js
@@ -1,66 +1,114 @@
 import React from 'react';
-import './incubation.css';
 import Promoting from "../../photos/prom.jpeg";
 import Generating from "../../photos/gen.jpeg"
-
+import { useState,useEffect } from 'react';
 function Incubation() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+    
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    console.log(windowWidth);
+    const containerStyle = {
+        width: windowWidth > 768 ? '100%' : windowWidth > 480 ? '100%' : '100%',
+        padding: '20px',
+        backgroundColor: '#e1b470',
+        lineHeight: '1.6',
+        color: '#4a3c2f',
+        fontSize: windowWidth > 768 ? '18px' : windowWidth > 480 ? '16px' : '14px',
+        textAlign: 'justify',
+        borderRadius: '10px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    };
+    
+    const headerStyle = {
+        textAlign: 'center',
+        fontSize: windowWidth > 768 ? '36px' : windowWidth > 480 ? '28px' : '24px',
+        color: '#6a4f4b',
+        paddingBottom: '10px',
+        textTransform: 'uppercase',
+    };
+    
+    const sectionStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        gap: '20px',
+        marginTop: '20px',
+        marginBottom: '20px',
+        flexDirection: windowWidth > 768 ? 'row' : 'column',
+    };
+    
+    const imageAndContentWidth = {
+        width: windowWidth > 768 ? '45%' : '100%',
+    };
+    
+    const imageStyle = {
+        ...imageAndContentWidth,
+        height:'400px',
+        objectFit: 'contain',
+        borderRadius: '8px',
+    };
+    
+    const contentStyle = {
+        ...imageAndContentWidth,
+        padding: windowWidth > 768 ? '20px' : windowWidth > 480 ? '15px' : '10px',
+        border: '1px solid #d3b82f',
+        borderRadius: '10px',
+        fontFamily: 'Georgia, serif',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    };
+    
+    
     return (
-        <div className="container">
-            <header className="header">
+        <div style={containerStyle}>
+            <header style={headerStyle}>
                 <h1>Technology Incubation Center</h1>
             </header>
-
-            <section className="focus-section">
-                <h2>Introduction</h2>
-                <p>
-                    A Technology Incubation Center (TIC) is a specialized facility designed to support and nurture early-stage technology startups and innovative projects.
-                    These centers provide a conducive environment for startups to develop their ideas, refine their business models, and bring their products to market.
-                    TICs play a vital role in fostering innovation, creating jobs, and contributing to the economic growth of a region.
-                </p>
-            </section>
-
-            <section className="focus-section">
+    
+            <div style={sectionStyle}>
+                <img src={Promoting} alt="Technology Centre" style={imageStyle} />
+                <div style={contentStyle}>
+                <p> A Technology Incubation Center (TIC) is a specialized facility designed to support and nurture early-stage technology startups and innovative projects.
+                        These centers provide a conducive environment for startups to develop their ideas, refine their business models, and bring their products to market.
+                        TICs play a vital role in fostering innovation, creating jobs, and contributing to the economic growth of a region.
+                     Many successful startups have emerged from Technology Incubation Centers around the world.
+                These startups have gone on to revolutionize their respective industries and create significant economic impact.
+                Technology Incubation Centers play a crucial role in fostering innovation and entrepreneurship.
+                        By providing the necessary support, resources, and mentorship, TICs help startups overcome challenges and achieve their full potential.
+                        They are essential for building a thriving innovation ecosystem and driving economic growth.
+    
+                        </p>
+                                    </div>
+            </div>
+            <div style={sectionStyle}>
+                
+                <div style={contentStyle}>
                 <h2>Key Features</h2>
-                <ul>
-                    <li><strong>Mentorship and Guidance:</strong> TICs offer access to experienced mentors and industry experts who provide valuable insights and guidance.
-                        Startups can benefit from one-on-one mentoring sessions, workshops, and networking events.</li>
-                    <li><strong>Infrastructure and Resources:</strong> These centers provide state-of-the-art infrastructure, including office space, laboratories, and prototyping facilities.
-                        Startups can utilize these resources to develop and test their products.</li>
-                    <li><strong>Funding and Investment:</strong> TICs often have connections with venture capitalists, angel investors, and funding agencies.
-                        They help startups secure the necessary funding to scale their operations and bring their products to market.</li>
-                    <li><strong>Collaborative Environment:</strong> Startups in TICs can collaborate with other like-minded entrepreneurs, researchers, and industry professionals.
-                        This collaborative environment fosters innovation and knowledge sharing.</li>
-                    <li><strong>Business Support Services:</strong> TICs offer a range of business support services, including legal, financial, and marketing assistance.
-                        These services help startups navigate the complexities of running a business and increase their chances of success.</li>
-                </ul>
-            </section>
-
-            <section className="image-section">
-                <img src={Promoting} alt="Promoting Entrepreneurship" className="image" />
-            </section>
-
-            <section className="focus-section">
-                <h2>Success Stories</h2>
-                <p>
-                    Many successful startups have emerged from Technology Incubation Centers around the world.
-                    These startups have gone on to revolutionize their respective industries and create significant economic impact.
-                </p>
-            </section>
-
-            <section className="image-section">
-                <img src={Generating} alt="Generating Employment" className="image" />
-            </section>
-
-            <section className="focus-section">
-                <h2>Conclusion</h2>
-                <p>
-                    Technology Incubation Centers play a crucial role in fostering innovation and entrepreneurship.
-                    By providing the necessary support, resources, and mentorship, TICs help startups overcome challenges and achieve their full potential.
-                    They are essential for building a thriving innovation ecosystem and driving economic growth.
-                </p>
-            </section>
+                    <ul>
+                        <li><strong>Mentorship and Guidance:</strong> TICs offer access to experienced mentors and industry experts who provide valuable insights and guidance.
+                            Startups can benefit from one-on-one mentoring sessions, workshops, and networking events.</li>
+                        <li><strong>Infrastructure and Resources:</strong> These centers provide state-of-the-art infrastructure, including office space, laboratories, and prototyping facilities.
+                            Startups can utilize these resources to develop and test their products.</li>
+                        <li><strong>Funding and Investment:</strong> TICs often have connections with venture capitalists, angel investors, and funding agencies.
+                            They help startups secure the necessary funding to scale their operations and bring their products to market.</li>
+                        <li><strong>Collaborative Environment:</strong> Startups in TICs can collaborate with other like-minded entrepreneurs, researchers, and industry professionals.
+                            This collaborative environment fosters innovation and knowledge sharing.</li>
+                        <li><strong>Business Support Services:</strong> TICs offer a range of business support services, including legal, financial, and marketing assistance.
+                            These services help startups navigate the complexities of running a business and increase their chances of success.</li>
+                    </ul>
+                                    </div>
+                                    <img src={Generating} alt="Technology Centre" style={imageStyle} />
+            </div>
+    
+          
+          
         </div>
     );
+    
 }
 
 export default Incubation;
