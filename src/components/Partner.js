6883293de34ba02d58ab46eba7b42f2ecf24e90a
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-01-02 15:04:51 +0530

    Update Partner.js

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -25,7 +25,7 @@ const Partner = () => {
     };
 
     try {
-      const response = await fetch("https://formspree.io/f/xovvkzrl", {
+      const response = await fetch(`${process.env.FORM}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
