bfa70d7d225516e8e90aa80d7143a68525db21c6
yathesth choudhary <109920623+yathe@users.noreply.github.com>
2025-04-30 00:28:30 +0530

    Update Getinvolved.js

--- src/components/Getinvolved.js
+++ src/components/Getinvolved.js
@@ -3,7 +3,6 @@ import React from 'react'
 import './Getinvolved.css'
 import partner from '../photos/partner.jpg'
 import vol from "../photos/volunteer.jpg"
-import { Link } from 'react-router-dom';
 const Getinvolved = () => {
   return (
     <div className='container12'><div className="text-container">
@@ -12,16 +11,16 @@ const Getinvolved = () => {
 <div className='grid1'>
   <div className='section1'>
   
-    <Link to="/volunter" className="link">
+ 
   <h5 className="label1">Volunteer  with  us</h5>
-</Link>
-<Link to="/volunter" className="link"><img src={vol} alt="Technology Demonstration" className="image1" /></Link>
+
+<img src={vol} alt="Technology Demonstration" className="image1" />
   </div>
   <div className='section1'>
-  <Link to="/partner" className="link">
+
   <h5 className="label1">Partner with us</h5>
-</Link>
-<Link to="/partner" className="link"> <img src={partner} alt="Technology Demonstration" className="image1" /></Link>
+
+<img src={partner} alt="Technology Demonstration" className="image1" />
   </div>
 
   </div>
